10cc9f6cc2b2b8eef643c9d0424c724ab9286210
vergaraCoder <jhonatanvergara072@gmail.com>
2024-10-02 11:29:45 -0500

    feat: se crea estructura de carpetas y archivo .env para enlazar base de datos

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CONSTRUCCION_AVANZADA_API_CON_.NET.Data
+{
+    public class ApplicationDbContext
+    {
+        
+    }
+}
